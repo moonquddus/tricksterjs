152d609543d9d23921c5ba17ed089fa302f7a9bb
Moon Quddus <moonquddus@gmail.com>
2017-09-11 19:30:51 +0100

    Did more save data parsing

--- service/save-service.js
+++ service/save-service.js
@@ -11,7 +11,6 @@ var saveService = {
 		// Won't stop cheats, but does the bare minimum in making sure the file isn't corrupted garbage
 		this.saveObj.saveHash = null;
 		this.saveObj.saveHash = JSON.stringify(this.saveObj).hashCode();
-		console.log("The Hash", this.saveObj.saveHash);
 
 		var saveData = new Blob([JSON.stringify(this.saveObj)], {type : 'application/json'});
 		var currentDate = new Date();
@@ -23,9 +22,21 @@ var saveService = {
 	},
 
 	processLoadedGame: function(fileContents){
-		var saveData = fileContents.target.result;
+
+		if (fileContents && fileContents.target){
+			var saveData = fileContents.target.result;
+		}
+		else{
+			errorService.throwError("Cannot load save file", "Save data not found");
+		}
 		// verify the game first
-		
+		var saveObj = JSON.parse(saveData);
+		if (typeOf(saveObj) == "object"){
+			console.log("Save Data", saveObj);
+		}
+		else{
+			errorService.throwError("Cannot load save file", "Could not convert save data");
+		}
 	}
 
 };
